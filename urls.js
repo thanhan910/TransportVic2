adbc5c18e33dbb9a95b659ae7b18a92fe2949815
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-03-11 14:57:42 +1100

    Add urls

---
+++ urls.js
@@ -0,0 +1,9 @@
+let url = 'https://data.ptv.vic.gov.au/downloads/gtfs.zip';
+
+let gtfsFeed = 'https://data.ptv.vic.gov.au/downloads/gtfs.zip';
+
+let metroWorkingTimetable = 'https://www.metrotrains.com.au/assets/Working-Timetable-%s.json';
+
+module.exports = {
+  url, gtfsFeed
+};
